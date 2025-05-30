da7b1c1dc51ff9b23b65f3232aa9f020e007f775
Matthew Behymer <beh20004@byui.edu>
2025-05-29 21:03:08 -0600

    Fix refresh token not sending accurate role. Update userDetails to send a response if no data is found.

--- controllers/userDetails.js
+++ controllers/userDetails.js
@@ -5,7 +5,11 @@ const fireDB = require('../db/connect').getDB();
 const getUserData = async (req, res) => {
     let id = req.params.id;
     const userDataRef = await fireDB.collection('userData').doc(id).get()
+    
+    if (!userDataRef.exists) return res.sendStatus('204') // No Content
     const userData = userDataRef.data();
+
+
     console.log('========================USER DATA RETRIEVED==================')
     console.log('userData', userData);
     res.json(userData);
