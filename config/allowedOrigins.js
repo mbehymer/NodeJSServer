a65e68eef01d213b08f057fe0100135fcd7a3ad4
Matthew Behymer <beh20004@byui.edu>
2025-07-02 23:05:45 -0600

    Allow mbehymer.github.io to be used as an allowed origin.

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
     'https://www.google.com',
     'http://localhost:8080',
     'http://localhost:5500',
-    'http://127.0.0.1:5500'
+    'http://127.0.0.1:5500',
+    'https://mbehymer.github.io'
 ];
 
 module.exports = allowedOrigins;
